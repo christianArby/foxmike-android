63ebc8c3d01f64a4c4ee8ba032e0a1632713d4f6
christianarby <26748445+christianArby@users.noreply.github.com>
2018-01-03 21:46:12 +0000

    Added list of requests in Requests fragment.

---
+++ app/src/main/java/com/example/chris/kungsbrostrand/UsersViewHolder.java
@@ -0,0 +1,59 @@
+package com.example.chris.kungsbrostrand;
+
+import android.graphics.Typeface;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+/**
+ * Created by chris on 2018-01-03.
+ */
+
+public class UsersViewHolder extends RecyclerView.ViewHolder {
+
+    View mView;
+
+    public UsersViewHolder(View itemView) {
+        super(itemView);
+
+        mView = itemView;
+    }
+
+    public void setHeading(String heading) {
+        TextView headingTV = (TextView) mView.findViewById(R.id.user_single_name);
+        headingTV.setText(heading);
+    }
+
+    public void setText(String text, Boolean normal) {
+        TextView userStatusView = (TextView) mView.findViewById(R.id.user_single_status);
+        userStatusView.setText(text);
+
+        if (!normal) {
+            userStatusView.setTypeface(userStatusView.getTypeface(), Typeface.BOLD);
+        } else {
+            userStatusView.setTypeface(userStatusView.getTypeface(), Typeface.NORMAL);
+        }
+    }
+
+    public void setUserImage(String thumb_image, android.content.Context context) {
+        CircleImageView userProfileImageIV = (CircleImageView) mView.findViewById(R.id.user_single_image);
+        Glide.with(context).load(thumb_image).into(userProfileImageIV);
+    }
+
+    public void setOnlineIcon(boolean userOnlineStatus) {
+
+        ImageView userOnlineView = (ImageView) mView.findViewById(R.id.user_single_online_iconIV);
+
+        if (userOnlineStatus) {
+            userOnlineView.setVisibility(View.VISIBLE);
+        } else {
+            userOnlineView.setVisibility(View.INVISIBLE);
+        }
+
+    }
+}
