c4519757e3ebd8b668f937c2ae03872f740162b3
christianarby <christianarby@23476e79-c1dd-4afc-b65e-7acd6a587d32>
2018-02-11 15:20:47 +0000

    Fixed bugs.

--- trunk/app/src/main/java/com/example/chris/kungsbrostrand/HostSessionsFragment.java
+++ trunk/app/src/main/java/com/example/chris/kungsbrostrand/HostSessionsFragment.java
@@ -21,14 +21,8 @@ import java.util.Calendar;
 import java.util.Collections;
 import java.util.Date;
 
-
-/**
- *
- */
 public class HostSessionsFragment extends Fragment {
 
-    private LinearLayout list1;
-    private OnSessionClickedListener onSessionClickedListener;
     private OnCreateSessionClickedListener onCreateSessionClickedListener;
     private FloatingActionButton createSessionBtn;
     private ViewPager hostSessionsPager;
@@ -59,11 +53,7 @@ public class HostSessionsFragment extends Fragment {
         /* Get the view fragment_user_account */
         final View view = inflater.inflate(R.layout.fragment_host_sessions, container, false);
 
-
-
-
         createSessionBtn = view.findViewById(R.id.add_session_btn);
-
         createSessionBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -85,38 +75,15 @@ public class HostSessionsFragment extends Fragment {
                     @Override
                     public void OnSessionsFound(ArrayList<Session> sessions) {
 
-                        ArrayList<Session> sessionsAdvWithin2weeks = new ArrayList<Session>();
-                        ArrayList<Session> sessionsAdvNotWithin2weeks = new ArrayList<Session>();
                         ArrayList<Session> sessionsAdv = new ArrayList<Session>();
                         ArrayList<Session> sessionsNotAdv = new ArrayList<Session>();
-                        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-                        final Calendar cal = Calendar.getInstance();
-                        Date todaysDate = cal.getTime();
-                        cal.add(Calendar.DATE,14);
-                        Date twoWeeksDate = cal.getTime();
-                        int n=0;
-                        int n1=0;
-                        int n2=0;
-                        for (Session session: sessions) {
 
+                        for (Session session: sessions) {
                             if (session.isAdvertised()) {
                                 sessionsAdv.add(session);
                             } else {
                                 sessionsNotAdv.add(session);
                             }
-
-                            /*if (session.isAdvertised()) {
-                                if (sessionDate.after(todaysDate) && sessionDate.before(twoWeeksDate)) {
-                                    sessionsAdvWithin2weeks.add(n, session);
-                                    n++;
-                                } else {
-                                    sessionsAdvNotWithin2weeks.add(n1, session);
-                                    n1++;
-                                }
-                            } else {
-                                sessionsNotAdv.add(n2, session);
-                            }*/
-
                         }
 
                         hostSessionsPager = (ViewPager) view.findViewById(R.id.host_sessions_pager);
@@ -141,7 +108,7 @@ public class HostSessionsFragment extends Fragment {
         return view;
     }
 
-    @Override
+    /*@Override
     public void onAttach(Context context) {
         super.onAttach(context);
         if (context instanceof OnSessionClickedListener) {
@@ -164,7 +131,7 @@ public class HostSessionsFragment extends Fragment {
         super.onDetach();
         onSessionClickedListener = null;
         onCreateSessionClickedListener = null;
-    }
+    }*/
 
     public interface OnCreateSessionClickedListener {
         void OnCreateSessionClicked();
