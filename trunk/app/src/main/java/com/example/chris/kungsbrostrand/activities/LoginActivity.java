9e95bbab680da7d0051ddf79ec41c5a98dc5a721
christianarby <christianarby@23476e79-c1dd-4afc-b65e-7acd6a587d32>
2018-03-03 14:39:11 +0000

    Test of moving files to subfolders.

--- trunk/app/src/main/java/com/example/chris/kungsbrostrand/activities/LoginActivity.java
+++ trunk/app/src/main/java/com/example/chris/kungsbrostrand/activities/LoginActivity.java
@@ -11,6 +11,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.chris.kungsbrostrand.activities.RegisterActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
