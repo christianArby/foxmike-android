777d22b40fc5228ec375d42c965821975ddaaefa
christianarby <26748445+christianArby@users.noreply.github.com>
2018-04-06 23:49:38 +0000

    Polished chat layout.

--- app/src/main/java/com/foxmike/android/adapters/MessageFirebaseAdapter.java
+++ app/src/main/java/com/foxmike/android/adapters/MessageFirebaseAdapter.java
@@ -1,6 +1,7 @@
 package com.foxmike.android.adapters;
 
 import android.graphics.Color;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,17 +12,23 @@ import com.bumptech.glide.Glide;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.foxmike.android.R;
+import com.foxmike.android.models.SessionDate;
 import com.foxmike.android.utils.GetTimeAgo;
 import com.foxmike.android.models.Message;
 import com.google.firebase.auth.FirebaseAuth;
+
+import java.security.Timestamp;
+import java.util.Calendar;
+import java.util.Date;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 /**
  * Created by chris on 2017-12-30.
  */
 
-public class MessageFirebaseAdapter extends FirebaseRecyclerAdapter<Message, MessageFirebaseAdapter.MessageViewHolder> {
+public class MessageFirebaseAdapter extends FirebaseRecyclerAdapter<Message, RecyclerView.ViewHolder> {
     boolean slalom;
+    FirebaseAuth mAuth;
     /**
      * This Firebase recycler adapter takes a firebase query and an boolean in order to populate a list of messages (chat).
      * If the boolean is true, the list is populated based on who sent the message. If current user has sent the message the message is shown to the right and
@@ -33,57 +40,106 @@ public class MessageFirebaseAdapter extends FirebaseRecyclerAdapter<Message, Mes
     }
 
     @Override
-    protected void onBindViewHolder(MessageViewHolder holder, int position, Message model) {
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view;
+        if (viewType==1) {
+            view = LayoutInflater.from(parent.getContext()).inflate(R.layout.this_message_single_layout,parent,false);
+            return new ThisMessageViewHolder(view);
+        } else {
+            view = LayoutInflater.from(parent.getContext()).inflate(R.layout.message_single_layout,parent,false);
+            return new OtherMessageViewHolder(view);
+        }
+    }
 
+    @Override
+    protected void onBindViewHolder(RecyclerView.ViewHolder holder, int position, Message model) {
         FirebaseAuth mAuth;
         String currentUserID;
         String fromUser = model.getSenderUserID();
         mAuth = FirebaseAuth.getInstance();
         currentUserID = mAuth.getCurrentUser().getUid();
 
-        holder.messageUser.setText(model.getSenderName());
-        Glide.with(holder.profileImage.getContext()).load(model.getSenderThumbImage()).into(holder.profileImage);
-
         if (fromUser.equals(currentUserID) && slalom) {
-            holder.singleMessageContainer.setLayoutDirection(View.LAYOUT_DIRECTION_RTL);
-            holder.messageText.setBackgroundResource(R.drawable.message_text_background_host);
-            holder.messageText.setTextColor(Color.WHITE);
-            holder.profileImage.setVisibility(View.GONE);
-            holder.messageUser.setVisibility(View.GONE);
-            holder.messageTime.setVisibility(View.GONE);
-        } else {
-            holder.singleMessageContainer.setLayoutDirection(View.LAYOUT_DIRECTION_LTR);
-            holder.messageText.setBackgroundResource(R.drawable.message_text_background);
-            holder.messageText.setTextColor(Color.BLACK);
-            holder.profileImage.setVisibility(View.VISIBLE);
-            holder.messageUser.setVisibility(View.VISIBLE);
-        }
+            ((ThisMessageViewHolder) holder).singleMessageContainer.setLayoutDirection(View.LAYOUT_DIRECTION_RTL);
+            ((ThisMessageViewHolder) holder).messageText.setBackgroundResource(R.drawable.message_text_background_host);
+            ((ThisMessageViewHolder) holder).messageText.setTextColor(Color.WHITE);
+            ((ThisMessageViewHolder) holder).messageText.setText(model.getMessage());
 
-        GetTimeAgo getTimeAgo = new GetTimeAgo();
-        String timeText = getTimeAgo.getTimeAgo(model.getTime(),holder.messageText.getContext());
-        holder.messageText.setText(model.getMessage());
-        holder.messageTime.setText(timeText);
+            Date d = new Date(model.getTime());
+            Calendar c = Calendar.getInstance();
+            c.setTime(d);
+            SessionDate sessionDate = new SessionDate(c);
+            String time = String.format("%02d:%02d", sessionDate.getHour(), sessionDate.getMinute());
+            String timeText = sessionDate.getDay() + " " + sessionDate.textMonth() +"." + " " + "KL." + " " + time;
+            ((ThisMessageViewHolder) holder).messageTime.setVisibility(View.VISIBLE);
+            ((ThisMessageViewHolder) holder).messageTime.setText(timeText);
+
+            if (position>0) {
+                long test = Math.abs(model.getTime()-getItem(position-1).getTime());
+                if (Math.abs(model.getTime()-getItem(position-1).getTime())<300000){
+                    //previous message is within 5 minutes of current message
+                    ((ThisMessageViewHolder) holder).messageTime.setVisibility(View.GONE);
+                }
+            }
+
+        } else {
+            Glide.with(((OtherMessageViewHolder) holder).profileImage.getContext()).load(model.getSenderThumbImage()).into(((OtherMessageViewHolder) holder).profileImage);
+            ((OtherMessageViewHolder) holder).singleMessageContainer.setLayoutDirection(View.LAYOUT_DIRECTION_LTR);
+            ((OtherMessageViewHolder) holder).messageText.setBackgroundResource(R.drawable.message_text_background);
+            ((OtherMessageViewHolder) holder).messageText.setTextColor(Color.BLACK);
+            ((OtherMessageViewHolder) holder).profileImage.setVisibility(View.VISIBLE);
+            ((OtherMessageViewHolder) holder).messageText.setText(model.getMessage());
+
+            Date d = new Date(model.getTime());
+            Calendar c = Calendar.getInstance();
+            c.setTime(d);
+            SessionDate sessionDate = new SessionDate(c);
+            String time = String.format("%02d:%02d", sessionDate.getHour(), sessionDate.getMinute());
+            String timeText = sessionDate.getDay() + " " + sessionDate.textMonth() +"." + " " + "KL." + " " + time;
+            ((OtherMessageViewHolder) holder).messageTime.setVisibility(View.VISIBLE);
+            ((OtherMessageViewHolder) holder).messageTime.setText(timeText);
+
+            if (position>0) {
+                if (Math.abs(model.getTime()-getItem(position-1).getTime())<300000){
+                    //previous message is within 5 minutes of current message
+                    ((OtherMessageViewHolder) holder).messageTime.setVisibility(View.GONE);
+                }
+            }
+        }
     }
 
     @Override
-    public MessageViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.message_single_layout,parent,false);
-        return new MessageViewHolder(view);
+    public int getItemViewType(int position) {
+        mAuth = FirebaseAuth.getInstance();
+        if (getItem(position).getSenderUserID().equals(mAuth.getCurrentUser().getUid())) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
 
-    public class MessageViewHolder extends RecyclerView.ViewHolder {
-        public TextView messageUser;
+    public class OtherMessageViewHolder extends RecyclerView.ViewHolder {
         public TextView messageText;
         public CircleImageView profileImage;
         public TextView messageTime;
         public RelativeLayout singleMessageContainer;
-        public MessageViewHolder(View view) {
+        public OtherMessageViewHolder(View view) {
             super(view);
-            messageUser = (TextView) view.findViewById(R.id.message_user_name);
             messageText = (TextView) view.findViewById(R.id.message_text);
             messageTime = (TextView) view.findViewById(R.id.message_time);
             profileImage = (CircleImageView) view.findViewById(R.id.message_profile_image);
+            singleMessageContainer = (RelativeLayout) view.findViewById(R.id.message_relative_layout);
+        }
+    }
+
+    public class ThisMessageViewHolder extends RecyclerView.ViewHolder {
+        public TextView messageText;
+        public TextView messageTime;
+        public RelativeLayout singleMessageContainer;
+        public ThisMessageViewHolder(View view) {
+            super(view);
+            messageText = (TextView) view.findViewById(R.id.message_text);
+            messageTime = (TextView) view.findViewById(R.id.message_time);
             singleMessageContainer = (RelativeLayout) view.findViewById(R.id.message_relative_layout);
         }
     }
