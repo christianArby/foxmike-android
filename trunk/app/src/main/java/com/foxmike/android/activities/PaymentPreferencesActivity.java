7fa5c8c59c7137900c250be0d864653da6f605d9
christianarby <christianarby@23476e79-c1dd-4afc-b65e-7acd6a587d32>
2018-06-06 07:24:49 +0000

    Fixed bugs.

--- trunk/app/src/main/java/com/foxmike/android/activities/PaymentPreferencesActivity.java
+++ trunk/app/src/main/java/com/foxmike/android/activities/PaymentPreferencesActivity.java
@@ -2,14 +2,37 @@ package com.foxmike.android.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.foxmike.android.R;
+import com.foxmike.android.adapters.ListPayoutMethodsAdapter;
+import com.google.firebase.functions.FirebaseFunctions;
 
 public class PaymentPreferencesActivity extends AppCompatActivity {
+
+    private FirebaseFunctions mFunctions;
+    private View mainView;
+    private RecyclerView listPaymentMethodsRV;
+    private ListPayoutMethodsAdapter listPaymentMethodsAdapter;
+    private ProgressBar progressBar;
+    private TextView addPaymentMethodTV;
+    private String stripeAccountId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_preferences);
+
+        mFunctions = FirebaseFunctions.getInstance();
+
+        mainView = findViewById(R.id.mainView);
+        progressBar = findViewById(R.id.progressBar_cyclic);
+        addPaymentMethodTV = findViewById(R.id.addPayoutMethodTV);
+        listPaymentMethodsRV = findViewById(R.id.listPayoutMethodsRV);
+
+
     }
 }
