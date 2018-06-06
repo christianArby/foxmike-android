40108ce5e282f5e98f1cb6ccd172524c66bd8195
christianarby <26748445+christianArby@users.noreply.github.com>
2018-06-06 20:05:58 +0000

    Added Payment preferences. Fixed bugs.

--- app/src/main/java/com/foxmike/android/activities/PaymentPreferencesActivity.java
+++ app/src/main/java/com/foxmike/android/activities/PaymentPreferencesActivity.java
@@ -1,26 +1,48 @@
 package com.foxmike.android.activities;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.foxmike.android.R;
-import com.foxmike.android.adapters.ListPayoutMethodsAdapter;
+import com.foxmike.android.adapters.ListPaymentMethodsAdapter;
+import com.foxmike.android.interfaces.OnPaymentMethodClickedListener;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.functions.FirebaseFunctions;
+import com.google.firebase.functions.FirebaseFunctionsException;
+import com.google.firebase.functions.HttpsCallableResult;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import static android.content.ContentValues.TAG;
 
 public class PaymentPreferencesActivity extends AppCompatActivity {
 
     private FirebaseFunctions mFunctions;
     private View mainView;
     private RecyclerView listPaymentMethodsRV;
-    private ListPayoutMethodsAdapter listPaymentMethodsAdapter;
+    private ListPaymentMethodsAdapter listPaymentMethodsAdapter;
     private ProgressBar progressBar;
     private TextView addPaymentMethodTV;
-    private String stripeAccountId;
+    private String stripeCustomerId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,11 +56,51 @@ public class PaymentPreferencesActivity extends AppCompatActivity {
         addPaymentMethodTV = findViewById(R.id.addPaymentMethodTV);
         listPaymentMethodsRV = findViewById(R.id.listPaymentMethodsRV);
 
-        addPaymentMethodTV.setOnClickListener(new View.OnClickListener() {
+        findStripeCustomer();
+    }
+
+    private void findStripeCustomer() {
+        listPaymentMethodsRV.setVisibility(View.INVISIBLE);
+        progressBar.setVisibility(View.VISIBLE);
+        addPaymentMethodTV.setVisibility(View.GONE);
+        DatabaseReference rootDbRef = FirebaseDatabase.getInstance().getReference();
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+        rootDbRef.child("users").child(mAuth.getCurrentUser().getUid()).child("stripeCustomerId").addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
-            public void onClick(View view) {
-                Intent createIntent = new Intent(PaymentPreferencesActivity.this,CreateStripeCustomerActivity.class);
-                startActivityForResult(createIntent,1);
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.getValue()!=null) {
+                    stripeCustomerId = dataSnapshot.getValue().toString();
+                    retrieveStripeCustomerSources(stripeCustomerId);
+                    addPaymentMethodTV.setText("Add payment method");
+                    addPaymentMethodTV.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            /*HashMap<String, Object> accountData = new HashMap<>();
+                            accountData.put("stripeCustomerId", stripeCustomerId);
+                            Intent updateIntent = new Intent(PayoutPreferencesActivity.this,UpdateStripeAccountWithPayoutActivity.class);
+                            updateIntent.putExtra("accountData",accountData);
+                            startActivityForResult(updateIntent, 1);*/
+
+                        }
+                    });
+                } else {
+                    addPaymentMethodTV.setText("Add payment method");
+                    // If no stripe account exist, show add payout method text
+                    addPaymentMethodTV.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            Intent createIntent = new Intent(PaymentPreferencesActivity.this,CreateStripeCustomerActivity.class);
+                            startActivityForResult(createIntent, 1);
+                        }
+                    });
+                    progressBar.setVisibility(View.GONE);
+                    addPaymentMethodTV.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
 
             }
         });
@@ -52,5 +114,99 @@ public class PaymentPreferencesActivity extends AppCompatActivity {
             startActivity(refresh);
             this.finish();
         }
+    }
+
+    private void retrieveStripeCustomerSources(String customerID) {
+        // Retrieve Stripe Account
+        retrieveStripeCustomer(customerID).addOnCompleteListener(new OnCompleteListener<HashMap<String, Object>>() {
+            @Override
+            public void onComplete(@NonNull Task<HashMap<String, Object>> task) {
+                // If not succesful, show error and return from function, will trigger if account ID does not exist
+                if (!task.isSuccessful()) {
+                    Exception e = task.getException();
+                    if (e instanceof FirebaseFunctionsException) {
+                        FirebaseFunctionsException ffe = (FirebaseFunctionsException) e;
+                        // Function error code, will be INTERNAL if the failure
+                        // was not handled properly in the function call.
+                        FirebaseFunctionsException.Code code = ffe.getCode();
+                        // Arbitrary error details passed back from the function,
+                        // usually a Map<String, Object>.
+                        Object details = ffe.getDetails();
+                    }
+
+                    progressBar.setVisibility(View.GONE);
+                    addPaymentMethodTV.setVisibility(View.VISIBLE);
+
+                    // [START_EXCLUDE]
+                    Log.w(TAG, "retrieve:onFailure", e);
+                    showSnackbar("An error occurred." + e.getMessage());
+                    return;
+                    // [END_EXCLUDE]
+                }
+
+
+                // If successful, extract
+                HashMap<String, Object> result = task.getResult();
+
+                if (result.get("resultType").toString().equals("sources")) {
+
+                    HashMap<String, Object> sources = (HashMap<String, Object>) result.get("sources");
+                    ArrayList<HashMap<String,Object>> sourcesDataList = (ArrayList<HashMap<String,Object>>) sources.get("data");
+
+                    String defaultSource = result.get("default_source").toString();
+
+                    listPaymentMethodsRV.setLayoutManager(new LinearLayoutManager(PaymentPreferencesActivity.this));
+                    listPaymentMethodsAdapter = new ListPaymentMethodsAdapter(sourcesDataList, PaymentPreferencesActivity.this, defaultSource, new OnPaymentMethodClickedListener() {
+                        @Override
+                        public void OnPaymentMethodClicked(String customerId) {
+                            showSnackbar(customerId + "Was clicked!");
+                        }
+                    });
+                    listPaymentMethodsRV.setAdapter(listPaymentMethodsAdapter);
+                    listPaymentMethodsRV.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+                        @Override
+                        public void onGlobalLayout() {
+                            //At this point the layout is complete and the
+                            //dimensions of recyclerView and any child views are known.
+                            addPaymentMethodTV.setVisibility(View.VISIBLE);
+                            progressBar.setVisibility(View.GONE);
+                            listPaymentMethodsRV.setVisibility(View.VISIBLE);
+                        }
+                    });
+
+
+
+
+
+                } else {
+                    HashMap<String, Object> error = (HashMap<String, Object>) result.get("error");
+                    showSnackbar(error.get("message").toString());
+                }
+                // [END_EXCLUDE]
+            }
+        });
+    }
+
+    // Function createStripeAccount
+    private Task<HashMap<String, Object>> retrieveStripeCustomer(String customerID) {
+
+        // Call the function and extract the operation from the result which is a String
+        return mFunctions
+                .getHttpsCallable("retrieveCustomer")
+                .call(customerID)
+                .continueWith(new Continuation<HttpsCallableResult, HashMap<String, Object>>() {
+                    @Override
+                    public HashMap<String, Object> then(@NonNull Task<HttpsCallableResult> task) throws Exception {
+                        // This continuation runs on either success or failure, but if the task
+                        // has failed then getResult() will throw an Exception which will be
+                        // propagated down.
+                        HashMap<String, Object> result = (HashMap<String, Object>) task.getResult().getData();
+                        return result;
+                    }
+                });
+    }
+
+    private void showSnackbar(String message) {
+        Snackbar.make(mainView, message, Snackbar.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/foxmike/android/interfaces/OnPaymentMethodClickedListener.java
@@ -0,0 +1,9 @@
+package com.foxmike.android.interfaces;
+
+/**
+ * Created by chris on 2018-06-06.
+ */
+
+public interface OnPaymentMethodClickedListener {
+    void OnPaymentMethodClicked(String customerId);
+}
